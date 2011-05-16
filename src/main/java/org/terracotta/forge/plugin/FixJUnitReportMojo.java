53073858e7074f1f34fbb964eed7d9802a90c280
hung huynh <hhuynh-unknown@terracottatech.com>
2011-05-16 21:49:52 +0000

    INT-1244: cleaned up junit report by removing properties and system-out system-err elements
    svn path=/projects/maven-forge-plugin/trunk/; revision=27935

--- src/main/java/org/terracotta/forge/plugin/FixJUnitReportMojo.java
+++ src/main/java/org/terracotta/forge/plugin/FixJUnitReportMojo.java
@@ -6,17 +6,12 @@ package org.terracotta.forge.plugin;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+import org.terracotta.forge.plugin.util.JUnitReportCleaner;
 
 /**
  * Fix zero-length junit reports
@@ -25,100 +20,54 @@ import org.apache.maven.project.MavenProject;
  * @goal fix-junit-report
  */
 public class FixJUnitReportMojo extends AbstractMojo {
-	/**
-	 * project instance. Injected automatically by Maven
-	 * 
-	 * @parameter expression="${project}"
-	 * @required
-	 * @readonly
-	 */
-	protected MavenProject project;
-	private final Pattern classnamePatternXml = Pattern
-			.compile("TEST-(.*)\\.xml");
-	private final Pattern classnamePatternTxt = Pattern.compile("(.*)\\.txt");
+  /**
+   * project instance. Injected automatically by Maven
+   * 
+   * @parameter expression="${project}"
+   * @required
+   * @readonly
+   */
+  protected MavenProject project;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		File sureFireReportDir = new File(project.getBuild().getDirectory(),
-				"surefire-reports");
-		if (!sureFireReportDir.isDirectory()) {
-			getLog().warn("surefire-reports folder was not found");
-		} else {
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    File sureFireReportDir = new File(project.getBuild().getDirectory(),
+        "surefire-reports");
+    if (!sureFireReportDir.isDirectory()) {
+      getLog().warn("surefire-reports folder was not found");
+    } else {
 
-			File[] reports = sureFireReportDir.listFiles(new FilenameFilter() {
-				public boolean accept(File dir, String name) {
-					return new File(dir, name).length() == 0L;
-				}
-			});
+      JUnitReportCleaner cleaner = new JUnitReportCleaner();
 
-			if (reports.length == 0) {
-				getLog().info("No empty junit reports were found");
-			}
+      File[] reports = sureFireReportDir.listFiles(new FilenameFilter() {
+        public boolean accept(File dir, String name) {
+          return (name.startsWith("TEST-") && name.endsWith(".xml"))
+              || name.endsWith(".txt");
+        }
+      });
 
-			for (File report : reports) {
-				String className = getClassname(report.getName());
+      if (reports.length == 0) {
+        getLog().info("No empty junit reports were found");
+      }
 
-				// skip non junit report
-				if (className == null) continue;
+      for (File report : reports) {
+        String className = cleaner.getClassname(report.getName());
 
-				getLog().info("Fixing report " + report);
+        // skip non junit report
+        if (className == null)
+          continue;
 
-				if (report.getName().endsWith(".xml")) {
-					try {
-						createDefaultReport(report, className);
-					} catch (IOException e) {
-						throw new MojoExecutionException(
-								"failed to write default report", e);
-					}
-				} else if (report.getName().endsWith(".txt")) {
-					File xmlReport = new File(report.getParentFile(), "TEST-"
-							+ className + ".xml");
-					if (!xmlReport.exists() || xmlReport.length() == 0L) {
-						try {
-							createDefaultReport(xmlReport, className);
-						} catch (IOException e) {
-							throw new MojoExecutionException(
-									"failed to write default report", e);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	private void createDefaultReport(File report, String className)
-			throws IOException {
-		String defaultReport = DEAULT_REPORT.replace("CLASSNAME", className);
-		Writer writer = null;
-		try {
-			writer = new PrintWriter(report);
-			IOUtils.write(defaultReport, writer);
-		} finally {
-			IOUtils.closeQuietly(writer);
-		}
-	}
-
-	private String getClassname(String filename) {
-		Matcher matcher = null;
-		if (filename.endsWith(".xml")) {
-			matcher = classnamePatternXml.matcher(filename);
-		} else if (filename.endsWith(".txt")) {
-			matcher = classnamePatternTxt.matcher(filename);
-		}
-
-		if (matcher.matches()) {
-			return matcher.group(1);
-		} else {
-			return null;
-		}
-	}
-
-	private static final String DEAULT_REPORT = "<?xml version='1.0' encoding='UTF-8'?>\n"
-			+ "<testsuite errors='0' failures='1' name='CLASSNAME' tests='1' time='0.000'>\n"
-			+ "  <testcase classname='CLASSNAME' name='test' time='0.0'>\n"
-			+ "    <failure type='junit.framework.AssertionFailedError' message='Failed'>\n"
-			+ "      Test has timeout or crashed. Please check logs for details.\n"
-			+ "    </failure>\n"
-			+ "  </testcase>\n"
-			+ "  <system-out />\n"
-			+ "  <system-err />\n" + "</testsuite>\n";
+        if (report.getName().endsWith(".xml")) {
+          getLog().debug("Cleaning up report " + report);
+          cleaner.cleanReport(report);
+        } else if (report.getName().endsWith(".txt")) {
+          File xmlReport = new File(report.getParentFile(), "TEST-" + className
+              + ".xml");
+          if (!xmlReport.exists() || xmlReport.length() == 0L) {
+            getLog().debug("Creating default report " + report);
+            cleaner.createDefaultReport(xmlReport, className);
+          }
+        }
+      }
+    }
+  }
 }
---
+++ src/main/java/org/terracotta/forge/plugin/util/JUnitReportCleaner.java
@@ -0,0 +1,215 @@
+/*
+ * All content copyright (c) Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package org.terracotta.forge.plugin.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.apache.commons.io.IOUtils;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * A tool to clean up junit report for elements that we're not interested in
+ * like "properties" "system-out" and "system-err"
+ * 
+ * If the report has syntax error or zero in length, the report will be replaced
+ * with a generic error report
+ * 
+ * @author hhuynh
+ * 
+ */
+public class JUnitReportCleaner extends DefaultHandler {
+  private final Set<String>   UNWANTED_ELEMENTS  = new HashSet<String>();
+  private final Pattern       CLASSNAME_FROM_XML = Pattern
+                                                     .compile("TEST-(.*)\\.xml");
+  private final Pattern       CLASSNAME_FROM_TXT = Pattern
+                                                     .compile("(.*)\\.txt");
+  private static final String DEAULT_REPORT      = "<?xml version='1.0' encoding='UTF-8'?>\n"
+                                                     + "<testsuite errors='0' failures='1' name='CLASSNAME' tests='1' time='0.000'>\n"
+                                                     + "  <testcase classname='CLASSNAME' name='test' time='0.0'>\n"
+                                                     + "    <failure type='junit.framework.AssertionFailedError' message='Failed'>\n"
+                                                     + "      Test has timeout or crashed. Please check logs for details.\n"
+                                                     + "    </failure>\n"
+                                                     + "  </testcase>\n"
+                                                     + "  <system-out />\n"
+                                                     + "  <system-err />\n"
+                                                     + "</testsuite>\n";
+
+  private StringBuilder       reportBuffer;
+  private StringBuilder       currentText;
+  private int                 deleteDepth;
+
+  private StringBuilder       text;
+
+  public JUnitReportCleaner() {
+    UNWANTED_ELEMENTS.add("properties");
+    UNWANTED_ELEMENTS.add("system-out");
+    UNWANTED_ELEMENTS.add("system-err");
+  }
+
+  public void cleanReport(File report) {
+    if (!report.exists()) {
+      throw new RuntimeException("JUnit report " + report + " doesn't exist");
+    }
+    if (report.length() == 0L) {
+      createDefaultReport(report, getClassname(report.getName()));
+      return;
+    }
+
+    reset(report);
+
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    SAXParser parser;
+    try {
+      parser = factory.newSAXParser();
+    } catch (ParserConfigurationException e) {
+      throw new RuntimeException(e);
+    } catch (SAXException e) {
+      throw new RuntimeException(e);
+    }
+
+    try {
+      parser.parse(report, this);
+      Writer writer = null;
+      try {
+        writer = new PrintWriter(report);
+        IOUtils.write(reportBuffer.toString(), writer);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      } finally {
+        IOUtils.closeQuietly(writer);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (SAXException e) {
+      createDefaultReport(report, getClassname(report.getName()));
+    }
+  }
+
+  public void createDefaultReport(File report, String className) {
+    String defaultReport = DEAULT_REPORT.replace("CLASSNAME", className);
+    Writer writer = null;
+    try {
+      writer = new PrintWriter(report);
+      IOUtils.write(defaultReport, writer);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      IOUtils.closeQuietly(writer);
+    }
+  }
+
+  public String getClassname(String filename) {
+    Matcher matcher = null;
+    if (filename.endsWith(".xml")) {
+      matcher = CLASSNAME_FROM_XML.matcher(filename);
+    } else if (filename.endsWith(".txt")) {
+      matcher = CLASSNAME_FROM_TXT.matcher(filename);
+    }
+
+    if (matcher.matches()) {
+      return matcher.group(1);
+    } else {
+      return null;
+    }
+  }
+
+  private void reset(File report) {
+    reportBuffer = new StringBuilder();
+    deleteDepth = 0;
+  }
+
+  @Override
+  public void startDocument() throws SAXException {
+    reportBuffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+  }
+
+  @Override
+  public void endDocument() throws SAXException {
+    // nothing yet
+  }
+
+  @Override
+  public void characters(char[] ch, int start, int length) throws SAXException {
+    if (deleteDepth == 0) {
+      String s = new String(ch, start, length);
+      if (s.trim().length() == 0)
+        return;
+      currentText.append(escapeXML(s));
+    }
+  }
+
+  @Override
+  public void startElement(String uri, String localName, String name,
+      Attributes attributes) throws SAXException {
+    currentText = new StringBuilder();
+
+    if (UNWANTED_ELEMENTS.contains(name)) {
+      deleteDepth++;
+    }
+
+    if (deleteDepth == 0) {
+      reportBuffer.append("<" + name + " ");
+      for (int i = 0; i < attributes.getLength(); i++) {
+        reportBuffer.append(attributes.getQName(i) + "=\""
+            + escapeXML(attributes.getValue(i)) + "\" ");
+      }
+      reportBuffer.append(">\n");
+    }
+  }
+
+  @Override
+  public void endElement(String uri, String localName, String name)
+      throws SAXException {
+    if (deleteDepth == 0) {
+      if (currentText != null && currentText.length() > 0) {
+        reportBuffer.append(text.toString()).append("\n");
+      }
+      reportBuffer.append("</" + name + ">\n");
+      currentText = null;
+    }
+
+    if (UNWANTED_ELEMENTS.contains(name)) {
+      deleteDepth--;
+    }
+  }
+
+  private static String escapeXML(String s) {
+    StringBuilder result = new StringBuilder();
+    StringCharacterIterator iterator = new StringCharacterIterator(s);
+    char character = iterator.current();
+    while (character != CharacterIterator.DONE) {
+      if (character == '<') {
+        result.append("&lt;");
+      } else if (character == '>') {
+        result.append("&gt;");
+      } else if (character == '\"') {
+        result.append("&quot;");
+      } else if (character == '\'') {
+        result.append("&#039;");
+      } else if (character == '&') {
+        result.append("&amp;");
+      } else {
+        result.append(character);
+      }
+      character = iterator.next();
+    }
+    return result.toString();
+  }
+}
