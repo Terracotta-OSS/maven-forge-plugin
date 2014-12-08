056c4fef37b52c032cd85a9073e1f82b90038658
tim wu <twu@terracottatech.com>
2014-12-08 21:42:27 +0000

    TAB-5334: Add goal for packaging clustered entities
    svn path=/projects/maven-forge-plugin/trunk/; revision=45871

--- src/main/java/org/terracotta/forge/plugin/AbstractResolveDependenciesMojo.java
+++ src/main/java/org/terracotta/forge/plugin/AbstractResolveDependenciesMojo.java
@@ -4,67 +4,21 @@
 package org.terracotta.forge.plugin;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.filter.CumulativeScopeArtifactFilter;
-import org.apache.maven.model.Repository;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectBuilder;
-import org.apache.maven.project.ProjectDependenciesResolver;
-import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder;
-import org.apache.maven.shared.dependency.graph.DependencyNode;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * abstract class to resolve dependencies of a given artifact
  * 
  * @author hhuynh
  */
-public abstract class AbstractResolveDependenciesMojo extends AbstractMojo {
-  private static final Pattern          MAVEN_COORDS_REGX = Pattern
-                                                              .compile("([^: ]+):([^: ]+)(:([^: ]*)(:([^: ]+))?)?:([^: ]+)");
-
-  @Component
-  private MavenProjectBuilder           mavenProjectBuilder;
-
-  @Component
-  private MavenProject                  project;
-
-  /**
-   * ArtifactRepository of the localRepository. To obtain the directory of localRepository in unit tests use
-   * System.getProperty("localRepository").
-   */
-
-  @Parameter(required = true, readonly = true, defaultValue = "${localRepository}")
-  private ArtifactRepository            localRepository;
-
-  /**
-   * The remote plugin repositories declared in the POM.
-   */
-  @Parameter(defaultValue = "${project.remoteArtifactRepositories}")
-  private List<Repository>              remoteRepositories;
-
-  @Component
-  protected ProjectDependenciesResolver projectDependenciesResolver;
-
-  @Component
-  private DependencyGraphBuilder        dependencyGraphBuilder;
-
-  @Component
-  private ArtifactFactory               defaultArtifactFactory;
+public abstract class AbstractResolveDependenciesMojo extends AbstractArtifactResolvingMojo {
 
   /**
    * artifact groupId:artifactId:version
@@ -121,43 +75,6 @@ public abstract class AbstractResolveDependenciesMojo extends AbstractMojo {
     return deps;
   }
 
-  private void getAllNodes(DependencyNode node, Set<DependencyNode> currentNodes) {
-    currentNodes.add(node);
-    for (DependencyNode currentNode : node.getChildren()) {
-      getAllNodes(currentNode, currentNodes);
-    }
-  }
-
-  protected Collection<Artifact> resolveArtifact(Artifact artifact) throws Exception {
-    Collection<Artifact> resolvedArtifacts = new ArrayList<Artifact>();
-    resolvedArtifacts.add(completeArtifact(artifact));
-    MavenProject pomProject = mavenProjectBuilder.buildFromRepository(artifact, remoteRepositories, localRepository);
-
-    // we start with the root node
-    DependencyNode rootNode = dependencyGraphBuilder.buildDependencyGraph(pomProject,
-                                                                          new CumulativeScopeArtifactFilter(Arrays
-                                                                              .asList(Artifact.SCOPE_COMPILE,
-                                                                                      Artifact.SCOPE_RUNTIME)));
-    Set<DependencyNode> nodes = new HashSet<DependencyNode>();
-    getAllNodes(rootNode, nodes);
-    nodes.remove(rootNode);
-    for (DependencyNode node : nodes) {
-      Artifact nodeArtifact = node.getArtifact();
-      Artifact completeArtifact = completeArtifact(nodeArtifact);
-      resolvedArtifacts.add(completeArtifact);
-    }
-
-    return resolvedArtifacts;
-  }
-
-  private Artifact completeArtifact(Artifact artifact) {
-    Artifact completeArtifact = defaultArtifactFactory.createArtifact(artifact.getGroupId(), artifact.getArtifactId(),
-                                                                      artifact.getBaseVersion(), artifact.getScope(),
-                                                                      artifact.getType());
-    completeArtifact.setFile(new File(localRepository.getBasedir(), localRepository.pathOf(completeArtifact)));
-    return completeArtifact;
-  }
-
   private void excludeGroupIds(Collection<Artifact> deps) {
     if (excludeGroupIds == null) { return; }
     Set<String> exclusions = new HashSet<String>();
@@ -212,25 +129,4 @@ public abstract class AbstractResolveDependenciesMojo extends AbstractMojo {
       }
     }
   }
-
-  private Artifact createArtifact(String coords) {
-    Matcher m = MAVEN_COORDS_REGX.matcher(coords);
-    if (!(m.matches())) { throw new IllegalArgumentException(
-                                                             "Bad artifact coordinates "
-                                                                 + coords
-                                                                 + ", expected format is <groupId>:<artifactId>[:<extension>[:<classifier>]]:<version>"); }
-
-    String groupId = m.group(1);
-    String artifactId = m.group(2);
-    String extension = get(m.group(4), "jar");
-    String classifier = get(m.group(6), "");
-    String version = m.group(7);
-    // ( String groupId, String artifactId, String version, String type, String classifier )
-    return defaultArtifactFactory.createArtifactWithClassifier(groupId, artifactId, version, extension, classifier);
-  }
-
-  private static String get(String value, String defaultValue) {
-    return (((value == null) || (value.length() <= 0)) ? defaultValue : value);
-  }
-
 }
---
+++ src/main/java/org/terracotta/forge/plugin/PackageClusteredEntityMojo.java
@@ -0,0 +1,235 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in the editor.
+ */
+package org.terracotta.forge.plugin;
+
+import edu.emory.mathcs.backport.java.util.Collections;
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Expand;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Used for clustered entity packaging:
+ * <ul>
+ * <li>Explodes embedded jars inside packaging dir</li>
+ * <li>List content of zip entries that contain exploded jars</li>
+ * <li>Rename .class files</li>
+ * <li>Unpacks API into the final clustered entity distribution package</li>
+ * <li>attaches API's javadoc to the distribution project</li>
+ * <li>create a record of all the artifacts shaded into the final jar</li>
+ * <ul>
+ * 
+ */
+@Mojo(name = "packageClusteredEntity", requiresDependencyResolution = ResolutionScope.COMPILE)
+public class PackageClusteredEntityMojo extends AbstractArtifactResolvingMojo {
+  @Component
+  protected MavenProject       project;
+
+  @Parameter(required = true)
+  private File                 packagingDir;
+
+  @Parameter(required = false)
+  private String               privateClassSuffix;
+
+  @Parameter(required = false, defaultValue = "server-content.txt")
+  private String               serverContentFilename;
+
+  @Parameter(required = false, defaultValue = "client-content.txt")
+  private String               clientContentFilename;
+
+  @Parameter(required = false, defaultValue = "false")
+  private boolean              skip;
+
+  @Parameter(required = true)
+  private String               serverArtifact;
+
+  @Parameter(required = true)
+  private String               clientArtifact;
+
+  @Parameter(required = true)
+  private String               apiArtifact;
+
+  @Parameter(required = false, defaultValue = "true")
+  private boolean              attachJavadoc;
+
+  @Parameter(required = false, defaultValue = "true")
+  private boolean              createShadeRecord;
+
+  public void execute() throws MojoExecutionException {
+    if (skip) {
+      getLog().info("Skipping clustered entity packaging");
+      return;
+    }
+    if (!packagingDir.exists()) throw new MojoExecutionException("Packaging dir not found: " + packagingDir);
+
+    ShadeRecordTracker shadeRecordTracker = new ShadeRecordTracker();
+    // Extract jars into respective folders
+    Artifact serverImpl = completeArtifact(createArtifact(serverArtifact));
+    shadeRecordTracker.add(serverImpl);
+    extractArtifactJarTo(new File(packagingDir, "server" + File.separator + serverImpl.getFile().getName()), serverImpl);
+    Artifact clientImpl = completeArtifact(createArtifact(clientArtifact));
+    shadeRecordTracker.add(clientImpl);
+    extractArtifactJarTo(new File(packagingDir, "client" + File.separator + clientImpl.getFile().getName()), clientImpl);
+    for (Artifact dependency : collectDependencies(serverImpl, clientImpl)) {
+      shadeRecordTracker.add(dependency);
+      extractArtifactJarTo(new File(packagingDir, "common" + File.separator + dependency.getFile()
+          .getName()), dependency);
+    }
+
+    String buildDirectory = project.getBuild().getOutputDirectory() + File.separator;
+    File serverEntriesFile = new File(packagingDir, serverContentFilename);
+    File clientEntriesFile = new File(packagingDir, clientContentFilename);
+
+    // convert .class into .clazz under embedded resources
+    if (privateClassSuffix != null) {
+      getLog().info("Renaming private classes to use suffix " + privateClassSuffix);
+      Set<String> commonResources = new LinkedHashSet<String>(renameResources(buildDirectory, "common"));
+      Set<String> clientResources = new LinkedHashSet<String>(renameResources(buildDirectory, "client"));
+      clientResources.addAll(commonResources);
+      Set<String> serverResources = new LinkedHashSet<String>(renameResources(buildDirectory, "server"));
+      serverResources.addAll(commonResources);
+
+      writeContentFile(serverEntriesFile, serverResources);
+      writeContentFile(clientEntriesFile, clientResources);
+    }
+
+    // Extract the contents of the API jar into the top level of the distribution jar
+    Artifact api = completeArtifact(createArtifact(apiArtifact));
+    shadeRecordTracker.add(api);
+    extractArtifactJarTo(packagingDir, api);
+
+    if (attachJavadoc) {
+      // Copy over the javadoc from the API package
+      Artifact apiJavadoc = getJavadocArtifact(api);
+      File javadocFile = new File(buildDirectory, project.getArtifactId() + "-" + project.getVersion() + "-javadoc.jar");
+      try {
+        FileUtils.copyFile(apiJavadoc.getFile(), javadocFile);
+      } catch (IOException e) {
+        throw new MojoExecutionException("Failed to copy javadoc", e);
+      }
+
+      // Create an artifact for javadocing the distribution package
+      Artifact javadocArtifact = defaultArtifactFactory.createArtifactWithClassifier(project.getGroupId(),
+          project.getArtifactId(), project.getVersion(), "jar", "javadoc");
+      javadocArtifact.setFile(javadocFile);
+
+      // Attach the copied javadoc to the project
+      project.addAttachedArtifact(javadocArtifact);
+    }
+    try {
+      shadeRecordTracker.writeTo(packagingDir);
+    } catch (FileNotFoundException e) {
+      throw new MojoExecutionException("Failed to create shade record", e);
+    }
+  }
+
+  private void extractArtifactJarTo(File dir, Artifact artifact) throws MojoExecutionException {
+    try {
+      getLog().info("Exploding " + artifact.getFile());
+      ensureMkdirs(dir);
+      unzip(dir, artifact.getFile());
+    } catch (IOException e) {
+      throw new MojoExecutionException("Failed to extra jar.", e);
+    }
+  }
+
+  private Set<Artifact> collectDependencies(final Artifact serverImpl, final Artifact clientImpl) throws MojoExecutionException {
+    Set<Artifact> dependencies = new HashSet<Artifact>();
+    try {
+      dependencies.addAll(resolveArtifact(serverImpl));
+      dependencies.addAll(resolveArtifact(clientImpl));
+    } catch (Exception e) {
+      throw new MojoExecutionException("Failed to collect depdendencies.", e);
+    }
+    dependencies.remove(serverImpl);
+    dependencies.remove(clientImpl);
+    return dependencies;
+  }
+
+  private void writeContentFile(final File entriesFilename, final Set<String> resources) throws MojoExecutionException {PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(entriesFilename);
+      for (String resource : resources) {
+        pw.println(resource);
+      }
+      pw.close();
+    } catch (IOException e) {
+      throw new MojoExecutionException("IO error", e);
+    } finally {
+      if (pw != null) {
+        pw.close();
+      }
+    }
+  }
+
+  private Set<String> renameResources(final String buildDirectory, final String subDir) {
+    final Iterator it;File dir = new File(packagingDir, subDir);
+    if (!dir.isDirectory()) return Collections.emptySet();
+    Set<String> renamed = new LinkedHashSet<String>();
+    it = FileUtils.iterateFiles(dir, null, true);
+    while (it.hasNext()) {
+      File resource = (File) it.next();
+      if (resource.getName().endsWith(".class")) {
+        File clazzFile = new File(resource.getParentFile(), resource.getName()
+            .replace(".class", privateClassSuffix));
+        resource.renameTo(clazzFile);
+        renamed.add(cleanupSuffix(buildDirectory, clazzFile.getAbsolutePath()));
+      } else {
+        renamed.add(cleanupSuffix(buildDirectory, resource.getAbsolutePath()));
+      }
+    }
+    return renamed;
+  }
+
+  private void unzip(File dest, File source) {
+    Project dummyProject = new Project();
+    dummyProject.init();
+    Expand unzip = new Expand();
+    unzip.setProject(dummyProject);
+    unzip.setDest(dest);
+    unzip.setSrc(source);
+    unzip.execute();
+  }
+
+  private void ensureMkdirs(File f) throws IOException {
+    if (!f.isDirectory() && !f.mkdirs()) throw new IOException("Failed to mkdirs " + f);
+  }
+
+  private static String cleanupSuffix(String suffix, String path) {
+    String cleaned = path.replace(suffix, "");
+    return cleaned.replace('\\', '/');
+  }
+
+  private class ShadeRecordTracker extends HashSet<Artifact> {
+    void writeTo(File dir) throws FileNotFoundException {
+      if (!createShadeRecord) {
+        return;
+      }
+      PrintWriter writer = new PrintWriter(new FileOutputStream(new File(dir, "shade-record.txt")));
+      try {
+        for (Artifact artifact : this) {
+          writer.println(artifact.toString());
+        }
+      } finally {
+        writer.close();
+      }
+    }
+  }
+}
